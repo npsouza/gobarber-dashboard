331a2c8c534ab75fad61737c8c39ce746807c258
Nathan <nathanpereirasouza@gmail.com>
2019-09-24 12:34:03 -0300

    estilizando notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '~/assets/logo-dashboard.svg';
 
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-dashboard.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
@@ -13,6 +15,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Prestador</strong>
