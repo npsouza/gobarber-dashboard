86dd58ef4da2f95b1472f22e0a41010826655bc1
Nathan <nathanpereirasouza@gmail.com>
2019-09-25 00:07:50 -0300

    estilizando dashboard, carregando agendamento

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding-bottom: 50px;
 `;
 
 export const Content = styled.div`
